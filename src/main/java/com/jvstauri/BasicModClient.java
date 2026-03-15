bfa6f345a403f81b85c41193f5fd4c0ad882393a
jvstauri <jvstauri@bufflo.edu>
2026-03-15 18:16:19 -0400

    initial commit

---
+++ src/main/java/com/jvstauri/BasicModClient.java
@@ -0,0 +1,8 @@
+package com.jvstauri.mhm;
+import net.fabricmc.api.ModInitializer;
+public class BasicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
---
+++ src/main/java/com/jvstauri/BasicModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jvstauri.mhm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BasicModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
